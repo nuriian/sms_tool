24693b60b9ccd4f0940144ef9fc0899877a5681c
nuriian <nuriian912@gmail.com>
2024-11-04 07:44:16 +0800

    Update sms_main.c

--- sms_main.c
+++ sms_main.c
@@ -374,7 +374,7 @@ alarm(10);
                     int skip_bytes;
 		    int sms_len;
 
-		    if (pdu && sms_txt && l > 0) {
+		    if (l > 0 && pdu && sms_time && phone_str && sms_txt && tp_dcs_type) {
                         sms_len = pdu_decode(pdu, l / 2, &sms_time, phone_str, sizeof(phone_str), sms_txt, sizeof(sms_txt), &tp_dcs_type, &ref_number, &total_parts, &part_number, &skip_bytes);
 		   	if (sms_len <= 0) {
                         	continue;
